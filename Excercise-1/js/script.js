68663aebf6aeaf57f7f21c9ed7829459e4f5fa6a
lKattt <117172826+lKattt@users.noreply.github.com>
2023-01-17 15:48:15 -0500

    Updated player controls

--- Excercise-1/js/script.js
+++ Excercise-1/js/script.js
@@ -8,27 +8,69 @@ author, and this description to match your project!
 
 "use strict";
 
+//score
+let score = 0;
 
 /**
-Description of preload
-*/
-function preload() {
+ Preload function
+ */
 
-}
+ let font;
 
 
 /**
 Description of setup
 */
 function setup() {
-    createCanvas(500, 500);
-    background(0, 255, 0);
+    createCanvas(1080, 720);
+    background(0, 0, 0);
+    font = loadFont('pixel.ttf');
 }
 
-
+// Paddles
+let paddle1 = 500;
+let paddle2 = 500;;
+    
+// The ball 
+let ball;
 /**
 Description of draw()
 */
 function draw() {
+    background(0);
 
-}
+    fill(255);
+    rect(40, paddle1, 50, 100);
+
+    fill(255);
+    rect(980, paddle2, 50, 100);
+// Press the corresponding key | ISSUES: Have to press un specified key for paddle to stop moving 
+    if (keyIsDown) { //When key is pressed and held it will continually move | ISSUES: each paddle stops moving when other paddle is moved
+        if (keyCode == 87) { // W
+            paddle1 -= 5;
+        } 
+        if (keyCode == 83) { // S
+            paddle1 += 5;
+        }
+        if (keyCode == 38) { // up arrow
+            paddle2 -= 5;
+        }
+        if (keyCode == 40) { // down arrow
+            paddle2 += 5;
+        }
+            print("working");
+        } 
+
+    textSize(50);
+    textFont(font);
+    text('PONG', 450, 50);
+    text('0', 150, 70);
+    text('0', 880, 70);
+ }
+
+ /** function scoreboard() {
+    if () {
+
+    }
+ }
+ **/
