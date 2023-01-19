3d3666d0601be69a044e8d3356d5852d11bfacf6
lKattt <117172826+lKattt@users.noreply.github.com>
2023-01-19 13:37:57 -0500

    Create script.js

---
+++ Excercise-2/js/script.js
@@ -0,0 +1,27 @@
+/**
+Pong
+by Katt for Cart 263 :)
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500, 500);
+    background(0, 0, 0);
+}
+
+
+/**
+Description of draw()
+*/
+//game screen
+function draw() {
+   
+}
+
+
+
+
