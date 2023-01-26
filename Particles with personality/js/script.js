0c9832a477e312f637da9135f102361851fa9b38
lKattt <117172826+lKattt@users.noreply.github.com>
2023-01-26 17:06:31 -0500

    update

--- Particles with personality/js/script.js
+++ Particles with personality/js/script.js
@@ -2,22 +2,17 @@
 Process of waking up
 Author: Katt
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+"use strict";
 */
 
-//"use strict";
-
-let ball = [];
+let ball;
 
 /**
 Description of setup
 */
 function setup() {
-    createCanvas(1080, 720);
-    for(let i = 0; i < 50; i++){ 
-        ball[i] = new Particle();
-    }
+    createCanvas(500, 720);
+    ball = new Particle();
 }
 
 
@@ -26,27 +21,26 @@ Description of draw()
 */
 function draw() {
     background(50);
-    for(let i = 0; i < ball.length; i++){ 
-        ball[i].move();
-        print(ball[i].x);
-    }
+    ball.display();
+    ball.move();
     
 }
 
 class Particle {
     constructor() {
-       this.x = random(width);
-       this.y = random(height);
-       this.speed = 5;
-       this.diameter = random(50);
-       this.speedMin = 2;
-       this.speedMax = 10;
+        this.x = width/2;
+        this.y = height/2;
+        this.speed = 2;
+    }
+    display() { 
+        fill(mouseX, mouseY, );
+        ellipse(this.x, this.y, 10);
     }
     move() {
-        this.speed = map(mouseY, 0, height, this.speedMin, this.speedMax);
         this.x += random(-this.speed, this.speed);
         this.y += random(-this.speed, this.speed);
-        fill(this.x, this.y, this.diameter, mouseY);
-        ellipse(this.x, this.y, this.diameter);
+    }
+    hover() {
+    
     }
 }
