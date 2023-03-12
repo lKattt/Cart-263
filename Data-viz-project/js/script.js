4e6fe0945ae76ff2ffa82443b0bd8847850fdec6
lKattt <117172826+lKattt@users.noreply.github.com>
2023-03-12 00:25:10 -0500

    update

--- Data-viz-project/js/script.js
+++ Data-viz-project/js/script.js
@@ -2,66 +2,58 @@ let table;
 let points = [];
 
 function preload() {
-  table = loadTable("Spotify 2010 - 2019 Top 100.csv", "csv", "header");
+  table = loadTable("Spotifytop.csv", "csv", "header");
 }
 
 function setup() {
   createCanvas(800, 800);
   background(0);
   for (var r = 0; r < table.getRowCount(); r++){ // Cycle through each row of the table
-      points[r] = new DataPoint(table.getString(r, 1), 
+      points[r] = new DataPoint(table.getString(r, 0), 
                                 table.getString(r, 2), 
-                                table.getString(r, 5), 
-                                table.getString(r, 0));
+                                table.getString(r, 4)); 
+                              //  table.getString(r, 7));
                                 // Pass through the values in each row
+  }
+  let rows = table.findRows('2010', 'year');
+  print(rows.length + ' Songs found');
+  for (let i = 0; i < rows.length; i++){
+  print(rows[i]);
   }
 }
 
 function draw() {
-  drawBasic();
+  //background(50);
+  for(let i = 0; i < 10; i++){
+    points[i].display();
 }
-class DataPoint { 
-    constructor(year, name, duration, ID){ 
-        // Add each data point to the object
-        this.year = year;
-        this.duration = duration;
-        this.name = name;
-        this.ID = ID;
-        this.x;
-        this.y;
-    }
+}
 
-    drawBasic(){ 
+class DataPoint { 
+    constructor(title, genre, bpm, year, ID){ 
+        // Add each data point to the object
+        this.title = title;
+        this.genre = genre;
+        this.bpm = bpm;
+        this.year = year;
+        this.ID = ID;
         this.x = random(width);
         this.y = random(height);
-        noStroke();
-        ellipse(random(width), random(height),int(this.duration)*3);
+        this.w = this.genre;
+        this.xspeed = 0;
+        this.yspeed = 0;
     }
+    display() {
+      noStroke();
+      fill(255);
+      ellipse(this.x, this.y, this.w);
+     // filter(BLUR, this.genre);
+    }
+   // move() {
 
-    drawCircle(){
-        this.radius = 150;
-        this.t=0;
-        this.angle = map(this.ID, 0, table.getRowCount(), 0, 1)*Math.PI*2;
-        this.x = Math.cos(this.angle)*this.radius+width/2;
-        this.y = Math.sin(this.angle)*this.radius+height/2;
-        noStroke();
-        fill(0, 200, 20, 40);
-        ellipse(this.x, this.y,int(this.duration)*3);
-        fill(0, 100, 200);
-        textSize(5);
-        push();
-        if(this.angle > Math.PI/2 && this.angle < Math.PI*1.5){
-          this.t = textWidth(this.name);
-          fill(255, 0,0);
-          translate(this.x, this.y);
-          rotate(this.angle+Math.PI);
-        } else { 
-          translate(this.x, this.y);
-          rotate(this.angle);
-        }
-        text(this.name, 0-this.t, 0);
-        pop();
-    }
+   // }
 }
+
+
 
 
