3d84eafac1c0308cf2ff3968d7b9ddd320e6263b
Katt <kurrypot45@gmail.com>
2023-03-12 03:08:56 -0400

    Update script.js

--- Data-viz-project/js/script.js
+++ Data-viz-project/js/script.js
@@ -15,6 +15,7 @@ function setup() {
                               //  table.getString(r, 7));
                                 // Pass through the values in each row
   }
+  filter(BLUR);
 }
 
 function draw() {
@@ -24,11 +25,12 @@ function draw() {
     //points[i].specify();
    // points[i].move();
 }
-background(10, 0.05);
+background(10, 0, 0, 0.05);
+//filter(BLUR, 10);
 }
 
 class DataPoint { 
-    constructor(title, genre, bpm, year, ID){ 
+    constructor(title, genre, bpm, year, ID){ //finds information stored under these catagories
         // Add each data point to the object
         this.title = title;
         this.genre = genre;
@@ -38,16 +40,12 @@ class DataPoint {
         this.x = random(0, width);
         this.y = random(0, height);
         this.w = random(this.genre);
-        this.xspeed = 2;
-        this.yspeed = 1;
     }
     display() { //displays particles using this.genre which returns values from the genre column | changes every refresh (not intended don't know why it does that but hey, something's better than nothing at this point)
       noStroke();
       fill(255);
       ellipse(this.x, this.y, this.genre);
-     // filter(BLUR, this.genre);
     // print(this.w);
-    print(this.bpm);
     }
    specify() { //finds bpm in rows that also have the year 2010 (or that's what it's supposed to do) | why does it only return one? 
    let row = table.findRows('2010', 'year');
